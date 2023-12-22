90ab9882f10ef9ee1bc4cff442acaeac60d63f05
BraveRam <99811385+BraveRam@users.noreply.github.com>
2023-12-22 19:46:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask import Flask, request
 TOKEN = '6064058058:AAFaGx_p7vqzEFiuxpdlSKTdEXdLdS9-pw0'  
 bot = telebot.TeleBot(TOKEN)
 
-webhook_url = 'https://flask-vercel-bot.vercel.app/webhook' 
+bot.remove_webhook()
+webhook_url = 'https://flask-vercel-bot.vercel.app/' 
 bot.set_webhook(url=webhook_url, drop_pending_updates=True)
 
 @bot.message_handler(func=lambda message: True)
@@ -13,7 +14,7 @@ def echo_all(message):
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['GET'])
+@app.route('/', methods=['POST'])
 def webhook():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode('utf-8'))])
     return 'ok', 200 
